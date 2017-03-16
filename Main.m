94df84379f931f38382852de93d325fc430c3f6b
Tim Murray <Tim Murray>
2017-03-16 16:39:25 -0500

    Main initializes random routers, began basic coverage function'

--- Main.m
+++ Main.m
@@ -8,5 +8,28 @@ cover = sum(topography(:)==0);  % Number of non-building spaces to be covered
 k = 50;                         % Number of routers
 range = 40;                     
 
+distances = ones(m,n)+Inf;
 
-imshow(top);
+% Randomly initialize router positions, then check that they are on open
+% spaces.
+routers = [randi(m,k,1),randi(n,k,1)];
+
+index = 1;
+while index <= k
+    if (topgraphy(routers(index,1),routers(index,2)) == 0)
+        index = index + 1;
+    else
+        routers(index,1) = randi(m);
+        routers(index,2) = randi(n);
+    end
+end
+
+% Anonymous function in order to pass the topography into objective
+% function separate from the router positions.
+% cvr = @(x) coverage(x,topgraphy)
+
+
+        
+
+
+%imshow(top);
---
+++ coverage.m
@@ -0,0 +1,7 @@
+function [ output_args ] = coverage( routers,topography,range )
+%Determines which squares in topgraphy are covered by a router using
+%   Dijkstra's algorithm.
+
+
+end
+
